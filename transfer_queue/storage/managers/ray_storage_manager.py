585f159c5a93c5c829326a73afa10ea7aa733ccf
Evelynn-V <liwenlin0223l@gmail.com>
2026-02-11 09:12:03 +0800

    fix
    Signed-off-by: Evelynn-V <liwenlin0223l@gmail.com>

--- transfer_queue/storage/managers/ray_storage_manager.py
+++ transfer_queue/storage/managers/ray_storage_manager.py
@@ -13,13 +13,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import logging
-import os
 from typing import Any
 
 from transfer_queue.storage.managers.base import KVStorageManager
 from transfer_queue.storage.managers.factory import TransferQueueStorageManagerFactory
 from transfer_queue.utils.zmq_utils import ZMQServerInfo
+
 
 @TransferQueueStorageManagerFactory.register("RayStore")
 class RayStorageManager(KVStorageManager):
