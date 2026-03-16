def924f78f07e5858eb3d90663661529a92e4a97
0oshowero0 <o0shower0o@outlook.com>
2026-03-16 15:59:47 +0800

    fix
    Signed-off-by: 0oshowero0 <o0shower0o@outlook.com>

--- transfer_queue/storage/managers/mooncake_manager.py
+++ transfer_queue/storage/managers/mooncake_manager.py
@@ -36,4 +36,5 @@ class MooncakeStorageManager(KVStorageManager):
             "Refer to https://github.com/kvcache-ai/Mooncake/issues/1645 for details."
         )
 
+        config["client_name"] = "MooncakeStorageClient"
         super().__init__(controller_info, config)
